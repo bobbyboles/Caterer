c98cea28065df12bca38ac45ff908b13f08e0a21
Bob <robert.dale.boles@gmail.com>
2022-12-18 21:07:47 -0700

    calendar added, testing func

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
 
 const Main = () => {
+   const [value, onChange] = useState(new Date())
     return (
-        <h1>Hello</h1>
-    )
-}
+        <>
+            <h1>Hello</h1>
+            <Calendar onChange={onChange} value={value}/>
+            {console.log(value)}
+        </>
+        
+    );
+};
 
-export default Main
+export default Main;
